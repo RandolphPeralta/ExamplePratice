1e9cc6f38806ce6c2631ad137f78c4332a4384d2
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-06-25 14:37:17 -0500

    Summary
    Summary

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
-const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
   entry: "./src/web.ts",
+
   output: {
-    filename: "bundle.js"
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
+
   module: {
     rules: [
       {
@@ -16,32 +17,15 @@ module.exports = {
       }
     ]
   },
+
   resolve: {
-    extensions: [".ts", ".js"],
-    fallback: {
-      fs: false,
-      path: false,
-      readline: false
-    }
+    extensions: [".ts", ".js"]
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      templateContent: `
-        <!DOCTYPE html>
-        <html>
-          <head><title>Hola Mundo</title></head>
-          <body>
-            <h1>hola mundo</h1>
-            <script src="bundle.js"></script>
-          </body>
-        </html>
-      `
-    }),
-    new webpack.DefinePlugin({
-      "process.env.ENVIRONMENT": JSON.stringify("WEB")
-    })
-  ],
+
   devServer: {
+    static: {
+      directory: path.join(__dirname, "src")
+    },
     open: true,
     port: 8000
   }

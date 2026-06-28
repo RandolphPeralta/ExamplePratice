35d0fecae241bf44f3fd17522916a2f27d7c81eb
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-06-28 14:54:13 -0500

    Summary
    Summary

--- webpack.config.js
+++ webpack.config.js
@@ -1,102 +1,32 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
-const webpack = require("webpack");
-const Dotenv = require("dotenv-webpack");
+const path = require('path');
 
-const webMode = {
-  name: "WebConfig",
-  entry: "./src/index.ts",
-  target: "web",
+module.exports = {
+  entry: "./src/web.ts",
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+
   resolve: {
     extensions: [".ts", ".js"]
   },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist/web")
-  },
-  externals: {
-    "prompt-sync": "{}"
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    }),
-    new Dotenv({
-      path: "./.env.local"
-    })
-  ],
+
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist/web")
+      directory: path.join(__dirname, "src")
     },
     open: true,
-    // port: 8000
-  },
-  mode: "production",
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/
-      }
-    ]
+    port: 8000
   }
 };
-
-const terminalMode = {
-  name: "TerminalConfig",
-  entry: "./src/index.ts",
-  target: "node",
-  resolve: {
-    extensions: [".ts", ".js"]
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist/terminal")
-  },
-  mode: "production",
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/
-      }
-    ]
-  }
-};
-
-module.exports = [webMode, terminalMode];
-
-
-// module.exports = {
-//   entry: "./src/web.ts",
-
-//   output: {
-//     filename: "bundle.js",
-//     path: path.resolve(__dirname, "dist")
-//   },
-
-//   module: {
-//     rules: [
-//       {
-//         test: /\.ts$/,
-//         use: "ts-loader",
-//         exclude: /node_modules/
-//       }
-//     ]
-//   },
-
-//   resolve: {
-//     extensions: [".ts", ".js"]
-//   },
-
-//   devServer: {
-//     static: {
-//       directory: path.join(__dirname, "src")
-//     },
-//     open: true,
-//     port: 8000
-//   }
-// };

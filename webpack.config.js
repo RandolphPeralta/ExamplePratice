a0628325b753e845cddc014e8cb069511f7004a7
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-07-06 23:06:04 -0500

    Summary
    Summary

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./src/index.ts",
+
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: [".ts", ".js"],
+        fallback: {
+            fs: false,
+            path: false,
+            readline: false
+        }
+    },
+
+    externals: {
+        "prompt-sync": "commonjs prompt-sync"
+    },
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "src/ui/web")
+        },
+        open: true,
+        port: 8000
+    }
+
+};

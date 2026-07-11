f9fc0bd0d52d083b7eabbf80a7760332ee93f4fc
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-07-10 20:29:00 -0500

    Summary
    Summary

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,50 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-    entry: "./src/index.ts",
-
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist")
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: "ts-loader",
-                exclude: /node_modules/
-            }
-        ]
-    },
-
-    resolve: {
-        extensions: [".ts", ".js"],
-        fallback: {
-            fs: false,
-            path: false,
-            readline: false
-        }
-    },
-
-    externals: {
-        "prompt-sync": "commonjs prompt-sync"
-    },
-
-    devServer: {
-        static: {
-            directory: path.join(__dirname, "src/ui/web")
-        },
-        open: true,
-        port: 8000
-    }
-
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      templateContent: `
+        <!DOCTYPE html>
+        <html>
+          <head><title>Sistema Biblioteca</title></head>
+          <body>
+            <div id="root"></div>
+            <script src="bundle.js"></script>
+          </body>
+        </html>
+      `
+    }),
+    new Dotenv({
+      path: "./.env.local" // aquí cargas tu entorno
+    }),
+    new webpack.DefinePlugin({
+      "process.env.PLATFORM": JSON.stringify(process.env.PLATFORM)
+    })
+  ],
+  devServer: {
+    open: true,
+    port: 8000
+  },
+  mode: "development"
 };
